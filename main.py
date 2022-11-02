e820eadb9c82ab883717facf05c294bcba4502e5
ronny <ronnyvu321@gmail.com>
2022-11-02 17:13:32 +0700

    Fix: Add timer for better scroll

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import csv
 import time
 
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver import FirefoxOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.support import expected_conditions as EC
@@ -22,6 +23,14 @@ firefox_driver = webdriver.Firefox(
 driver = firefox_driver
 # chrome_driver.get("https://edu2review.com/")
 driver.get("https://edu2review.com/")
+driver_opts = FirefoxOptions()
+driver_opts.add_argument("--incognito")
+driver_opts.add_argument("headless")
+
+driver_opts.add_argument("no-sandbox")
+driver_opts.add_argument("--disable-gpu")
+driver_opts.add_argument("--disable-dev-shm-usage")
+driver_opts.add_argument("no-first-run")
 
 menu = driver.find_element(
     By.CSS_SELECTOR, "li.desktop-navmenu__list-item:nth-child(5) > a:nth-child(1)"
@@ -54,16 +63,18 @@ LOADING_ELEMENT_XPATH = "//*[@id='load-more']"
 
 
 def get_school_list():
-    currentURL = driver.current_url
+    # currentURL = driver.current_url
+    last_height = driver.execute_script("return document.body.scrollHeight")
     match = False
-    while match == False:
-        lastURL = currentURL
+    while match is False:
+        # lastURL = currentURL
 
-        print("Last URL: " + lastURL)
+        # print("Last URL: " + lastURL)
+        print(last_height)
 
         print("scroll down")
         driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-        time.sleep(1)
+        time.sleep(2)
 
         try:
 
@@ -72,19 +83,26 @@ def get_school_list():
                 EC.element_to_be_clickable((By.XPATH, LOADING_ELEMENT_XPATH))
             )
             print("Finished")
+            time.sleep(1)
+            # currentURL = driver.current_url
 
         except TimeoutException:
             # if timeout exception was raised - it may be safe to
             # assume loading has finished, however this may not
             # always be the case, use with caution, otherwise handle
             # appropriately.
+            print("No more load bar or takes too long to load")
             pass
 
-        currentURL = driver.current_url
-        print("current URL: " + currentURL)
+        break
+
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        print(new_height)
         print("--------------------------------------------------")
-        if lastURL == currentURL:
+        if new_height == last_height:
             match = True
+        last_height = new_height
+        time.sleep(2)
 
     schools = driver.find_elements(
         By.XPATH, "//a[@class='mdc-card card-list-item fluid']"
@@ -92,45 +110,77 @@ def get_school_list():
     for school in schools:
         if school not in href_list:
             href_list.append(school.get_attribute("href"))
+    print(len(href_list))
 
 
 def find_user_ratings():
+
     for href in href_list:
         driver.get(href)
 
-        # Load more
-        load_more_button = driver.find_element(
-            By.CSS_SELECTOR,
-            "#general-review > div.rating-group__grouping-block > div > button",
-        )
+        last_height = driver.execute_script("return document.body.scrollHeight")
+        match = False
+        while match is False:
 
-        load_more_button.click()
+            print(last_height)
 
-        try:
+            try:
+                if driver.find_element(By.XPATH, "//*[@id='internal-popup-1144']"):
+                    driver.find_element(By.XPATH, "//*[@id='internal-popup-1144']").click()
+                    whandle = driver.window_handles[1]
+                    driver.switch_to.window(whandle)
+                    driver.close()
 
-            print("Waiting to load more comments")
-            WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable(
-                    (
-                        By.CSS_SELECTOR,
-                        "#general-review > div.rating-group__grouping-block > div > button",
+            except NoSuchElementException:
+
+                pass
+
+            print("scroll down")
+
+            # Load more
+            load_more_button = driver.find_element(
+                By.CSS_SELECTOR,
+                "#general-review > div.rating-group__grouping-block > div > button",
+            )
+
+            load_more_button.click()
+            time.sleep(2)
+            try:
+
+                print("Waiting to load more comments")
+                WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable(
+                        (
+                            By.CSS_SELECTOR,
+                            "#general-review > div.rating-group__grouping-block > div > button",
+                        )
                     )
                 )
-            )
-            print("Finishes")
+                print("Finishes")
 
-        except TimeoutException:
-            # if timeout exception was raised - it may be safe to
-            # assume loading has finished, however this may not
-            # always be the case, use with caution, otherwise handle
-            # appropriately.
-            pass
+            except TimeoutException:
+                # if timeout exception was raised - it may be safe to
+                # assume loading has finished, however this may not
+                # always be the case, use with caution, otherwise handle
+                # appropriately.
+                pass
+
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            print(new_height)
+            print("--------------------------------------------------")
+            if new_height == last_height:
+                match = True
+            last_height = new_height
+            time.sleep(2)
 
         ratings = driver.find_elements(By.CLASS_NAME, "comment-block")
         for rates in ratings:
 
             # print(rates.get_attribute("innerHTML"))
+            print("-------------------------------------------")
+            print(rates.get_attribute("id"))
             user_list.append(rates.get_attribute("id"))
+
             comment_block = rates.find_element(By.CLASS_NAME,
                                                "comment-block__content")
             comments = comment_block.text
@@ -140,6 +190,7 @@ def find_user_ratings():
 
             for r in read_more_comment:
                 inner = r.get_attribute("innerHTML")
+                print(inner)
                 read_more_body = ""
                 read_more_title = ""
 
@@ -149,13 +200,16 @@ def find_user_ratings():
                     read_more_body = inner
 
                 comments += read_more_title + read_more_body
+                # print(comments)
                 comment_list.append(comments)
+                time.sleep(1)
 
+        time.sleep(1)
     with open("scraped.txt", mode="w+", encoding="utf-8") as f:
         writer = csv.writer(f, delimiter=";")
         writer.writerow(["ID", "COMMENT"])
         for user, comment in zip(user_list, comment_list):
-            print(user + " - " + comment)
+            # print(user + " - " + comment)
             writer.writerow([user, comment])
 
 
